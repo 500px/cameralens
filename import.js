9344e99f06fc740887d77eab46cd6250645eb710
Ankit Sardesai <amsardesai@gmail.com>
2014-11-07 01:34:22 -0500

    should work

--- import.js
+++ import.js
@@ -1,3 +1,7 @@
+(function() {
+
+
+
 var mongojs = require('mongojs');
 var fs = require('fs');
 var csv = require('ya-csv');
@@ -7,10 +11,9 @@ var csv = require('ya-csv');
 var db_url = process.env.MONGOHQ_URL || 'mongodb://localhost:27020/cameralens'; 
 var db = mongojs(db_url, ['photos', 'cameras', 'lenses']);
 
-db.cameras.ensureIndex( {name: 1}, {unique: true} );
-db.lenses.ensureIndex( {name: 1}, {unique: true} );
-db.photos.ensureIndex( {camera: 1} );
-db.photos.ensureIndex( {lens: 1} );
+db.cameras.ensureIndex({ name: 1 }, { unique: true, background: true });
+db.lenses.ensureIndex({ name: 1 }, { unique: true, background: true });
+db.photos.ensureIndex({ camera: 1, lens: 1}, { background: true });
 
 // build camera data normalization dictionary
 cameraNormalize = JSON.parse(fs.readFileSync("refine_cluster.json", "utf8")); 
@@ -19,22 +22,45 @@ dictionary = {}
 cameraNormalize.forEach(function(column) {
   column.edits.forEach(function(type) {
     type.from.forEach(function(from) {
-      dictionary[from] = type.to.trim();
+      dictionary[from] = String(type.to);
     });
   });
 });
 
+
+
 var createCamera = function(cameraName, lensDoc, callback) {
-  db.cameras.update({name: cameraName}, {'$setOnInsert': { name: cameraName }, '$addToSet': {lenses: lensDoc._id}}, {upsert: true}, function() {
+  db.cameras.update({
+    name: cameraName
+  }, {
+    '$setOnInsert': {
+      name: cameraName
+    }, '$addToSet': {
+      lenses: lensDoc._id
+    }, '$inc': {
+      photoCount: 1
+    }
+  }, {upsert: true}, function() {
     db.cameras.findOne({name: cameraName}, function(err, doc) {
+      if (err) console.log("CAMERA ERROR!", err);
       callback(err, doc);
     })
   });
 }
 
 var createLens = function(lensName, callback) {
-  db.lenses.update({name: lensName}, {'$setOnInsert': { name: lensName }}, {upsert: true}, function() {
+  db.lenses.update({
+    name: lensName
+  }, {
+    '$setOnInsert': { 
+      name: lensName
+    }, '$inc': {
+      photoCount: 1
+    }
+  }, {upsert: true}, function() {
     db.lenses.findOne({name: lensName}, function(err, doc) {
+      if (err) console.log("LENSES ERROR!", err);
+      if (doc == null) console.log("IZ NULL!!!", err);
       callback(err, doc);
     });
   });
@@ -44,12 +70,13 @@ var count = 0;
 var finished = false;
 var total = 0;
 
-var reader = csv.createCsvFileReader('data/photo_meta.csv.small', 
-                                     {columnsFromHeader:true, 
-                                     'separator': ',',
+var reader = csv.createCsvFileReader('photo_meta.csv.small', 
+                                     {'separator': ',',
                                      'quote':'"',
                                      'escape':'\\',
                                      'comment':''});
+
+reader.setColumnNames(['id', 'camera', 'lens', 'focal_length', 'iso', 'shutter_speed', 'aperture', 'highest_rating']);
 
 reader.addListener('data', function(data){
 
@@ -58,11 +85,11 @@ reader.addListener('data', function(data){
   // get normalized camera and lens
   var cameraName = dictionary[data.camera];
   if (cameraName == null) {
-    cameraName = data.camera.trim();
+    cameraName = String(data.camera).trim();
   }
   var lensName = dictionary[data.lens];
   if (lensName == null) {
-    lensName = data.lens.trim();
+    lensName = String(data.lens).trim();
   }
  
   createLens(lensName, function(err, lensDoc) {
@@ -77,9 +104,9 @@ reader.addListener('data', function(data){
         aperture: data.aperture,
         highest_rating: data.highest_rating
       }, function(err, doc) {
-        if (++count % 10000 == 0) console.log(count);
+        if (++count % 1000 == 0 || count == 1) console.log("Done processing photo #", count);
         if (finished && count == total) {
-          console.log("FINISHED");
+          console.log("FINISHED processing photos!");
           process.exit(0);
         }
       });
@@ -110,3 +137,8 @@ reader.addListener('end', function(){
   process.exit(0);
 });
 */
+
+
+
+
+}).call(this);
--- routes/camera.js
+++ routes/camera.js
@@ -10,36 +10,36 @@ var db_url = process.env.MONGOHQ_URL || 'mongodb://localhost:27020/cameralens';
 var db = mongojs(db_url, ['photos', 'cameras', 'lenses',]);
 
 router.get('/', function(req, res) {
-  // find everything
-  db.cameras.find(function(err, cameras) {
+  getCameras(function(err, cameras) {
       // docs is an array of all the documents in mycollection
       res.render('home', { cameras: cameras || []  });
   });
   
 });
 
-
-var CAMERAS = [];
 function getCameras(callback){
-  if(CAMERAS.length) return callback(CAMERAS);
-
-  db.cameras.find({}, function(err, cameras) { 
-      CAMERAS = cameras; 
-      callback(CAMERAS)
-  }); 
+  db.cameras.find().sort({
+    photoCount: -1
+  }).limit(10, callback); 
 }
 
+function getLenses(camera, callback) {
+  db.cameras.findOne({name: camera}, function(err, docs) {
+    db.lenses.find({
+      _id: { '$in': docs.lenses }
+    }, callback);
+  });
+}
 
 function getPhotoset(camera, lens, callback){
   // lens is optional
   var find = {camera: camera}
-  if(lens) find.lens = lens;
+  if (lens) find.lens = lens;
 
-  db.photos.find(find).limit(50, function(err, docs){
-    callback(docs);
-  });
+  db.photos.find(find).limit(50, callback);
   // return 50 photos, ordere by DESC 
 }
+
 
 
 
@@ -101,107 +101,9 @@ router.get('/camera/:name', function(req, res) {
 
 
 
-   var mongojs = require('mongojs');
-var fs = require('fs');
-var csv = require('ya-csv');
-
-// DB setup
-
-var db_url = process.env.MONGOHQ_URL || 'mongodb://localhost:27020/cameralens'; 
-var db = mongojs(db_url, ['photos', 'cameras', 'lenses']);
 
 router.get('/import', function(req, res) {
-
-
-db.cameras.ensureIndex( {name: 1}, {unique: true} );
-db.lenses.ensureIndex( {name: 1}, {unique: true} );
-db.photos.ensureIndex( {camera: 1} );
-db.photos.ensureIndex( {lens: 1} );
-
-// build camera data normalization dictionary
-cameraNormalize = JSON.parse(fs.readFileSync("refine_cluster.json", "utf8")); 
-dictionary = {}
-
-cameraNormalize.forEach(function(column) {
-  column.edits.forEach(function(type) {
-    type.from.forEach(function(from) {
-      dictionary[from] = type.to.trim();
-    });
-  });
-});
-
-var createCamera = function(cameraName, lensDoc, callback) {
-  db.cameras.update({name: cameraName}, {'$setOnInsert': { name: cameraName }, '$addToSet': {lenses: lensDoc._id}}, {upsert: true}, function() {
-    db.cameras.findOne({name: cameraName}, function(err, doc) {
-      callback(err, doc);
-    })
-  });
-}
-
-var createLens = function(lensName, callback) {
-  db.lenses.update({name: lensName}, {'$setOnInsert': { name: lensName }}, {upsert: true}, function() {
-    db.lenses.findOne({name: lensName}, function(err, doc) {
-      callback(err, doc);
-    });
-  });
-}
-
-var count = 0;
-var finished = false;
-var total = 0;
-
-var reader = csv.createCsvFileReader('data/photo_meta.csv.small', 
-                                     {columnsFromHeader:true, 
-                                     'separator': ',',
-                                     'quote':'"',
-                                     'escape':'\\',
-                                     'comment':''});
-
-reader.addListener('data', function(data){
-
-  ++total;
-
-  // get normalized camera and lens
-  var cameraName = dictionary[data.camera];
-  if (cameraName == null) {
-    cameraName = data.camera.trim();
-  }
-  var lensName = dictionary[data.lens];
-  if (lensName == null) {
-    lensName = data.lens.trim();
-  }
- 
-  createLens(lensName, function(err, lensDoc) {
-    createCamera(cameraName, lensDoc, function(err, cameraDoc) {
-      db.photos.insert({
-        camera: cameraDoc._id,
-        lens: lensDoc._id,
-        id: data.id,
-        focal_length: data.focal_length,
-        iso: data.iso,
-        shutter_speed: data.shutter_speed,
-        aperture: data.aperture,
-        highest_rating: data.highest_rating
-      }, function(err, doc) {
-        if (++count % 10000 == 0) console.log(count);
-        if (finished && count == total) {
-          console.log("FINISHED");
-          process.exit(0);
-        }
-      });
-
-    });
-  });
-
-
-}); 
-
-reader.addListener('end', function(){
-  finished = true;
-});
-
-
-
+  require('../import.js');
 });
 
 
