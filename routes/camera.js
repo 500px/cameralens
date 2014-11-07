b8b257778e13b9f796e9014bb5c192fcbfca1f20
Ankit Sardesai <amsardesai@gmail.com>
2014-11-07 15:44:31 -0500

    commits

--- routes/camera.js
+++ routes/camera.js
@@ -3,11 +3,13 @@ var router = express.Router();
 var mongojs = require('mongojs');
 var https = require('https');
 var csv = require('ya-csv');
+var _ = require('underscore');
 
 // DB setup
 
 var db_url = process.env.MONGOHQ_URL || 'mongodb://localhost:27020/cameralens'; 
 var db = mongojs(db_url, ['photos', 'cameras', 'lenses',]);
+
 
 
 function getCameras(callback){
@@ -16,21 +18,47 @@ function getCameras(callback){
   }).limit(10, callback); 
 }
 
+function getCamera(camera, callback){
+  db.cameras.findOne({name: camera}, {lenses: 0}, callback); 
+}
+
+function getLens(lens, callback){
+  db.lenses.findOne({name: lens}, callback); 
+}
+
 function getLenses(camera, callback) {
   db.cameras.findOne({name: camera}, function(err, docs) {
     db.lenses.find({
-      _id: { '$in': docs.lenses }
-    }).sort().limit(20, callback);
+      name: { '$in': docs.lenses }
+    }).sort({photoCount: -1}).limit(20, callback);
   });
 }
 
 function getPhotoset(camera, lens, callback){
-  // lens is optional
-  var find = {camera: camera}
-  if (lens) find.lens = lens;
-
-  db.photos.find(find).sort({highest_rating: -1}).limit(10, callback);
-  // return 50 photos, ordere by DESC 
+  db.photos.find({camera: camera, lens: lens}).sort({highest_rating: -1}).limit(10, function(err, photos) {
+    var count = 0;
+    photos.forEach(function(photo) {
+      console.log('Getting photo', photo.id);
+      var req = https.request({
+        host: 'api.500px.com',
+        port: 443,
+        path: '/v1/photos/' + photo.id + '?consumer_key=5258tqxlxoTBAdE3sAm2yA43eo5in6r8fSTqyEMt',
+        method: 'GET'
+      }, function(res) {
+        res.on('data', function(data) {
+          count++;
+          var info = JSON.parse(data);
+          photo.src = info.photo.image_url;
+          photo.url = 'https://500px.com' + info.photo.url;
+          if (count == photos.length) callback(err, photos);
+        });
+      });
+      req.end();
+      req.on('error', function(error) {
+        console.log("ERROR!!!!!!!!!", error);
+      });
+    });
+  });
 }
 
 function getPhotosFromCamera(camera, callback) {
@@ -49,10 +77,7 @@ function getPhotosFromCamera(camera, callback) {
           var info = JSON.parse(data);
           photo.src = info.photo.image_url;
           photo.url = 'https://500px.com' + info.photo.url;
-          if (count == photos.length) {
-            console.log(photos);
-            callback(err, photos);
-          }
+          if (count == photos.length) callback(err, photos);
         });
       });
       req.end();
@@ -63,42 +88,20 @@ function getPhotosFromCamera(camera, callback) {
   });
 }
 
-/*
-function getPhotoDetail(id, cb){
-  api.getPhoto(cb)
+
+function getFocalLengths(lens, callback) {
+  db.photos.aggregate({
+    '$match': {lens: lens}
+  }, {
+    '$group': {_id: "$focal_length", count: { '$sum': 1 }}
+  }, function(err, docs) {
+    docs = _.filter(docs, function(obj) { return obj._id != '' && !/^\d*\.0$/.test(obj._id) });
+    docs = _.map(docs, function(obj) { obj._id = parseInt(obj._id, 10); return obj });
+    docs = _.sortBy(docs, function(obj) { return obj._id });
+    callback(err, docs);
+  });
 }
 
-
-
-function getPhotoStats(camera, lens){
-
-  // specs: http://www.chartjs.org/docs/
-  var data = {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
-    datasets: [
-        {
-            label: "My First dataset",
-            fillColor: "rgba(220,220,220,0.5)",
-            strokeColor: "rgba(220,220,220,0.8)",
-            highlightFill: "rgba(220,220,220,0.75)",
-            highlightStroke: "rgba(220,220,220,1)",
-            data: [65, 59, 80, 81, 56, 55, 40]
-        },
-        {
-            label: "My Second dataset",
-            fillColor: "rgba(151,187,205,0.5)",
-            strokeColor: "rgba(151,187,205,0.8)",
-            highlightFill: "rgba(151,187,205,0.75)",
-            highlightStroke: "rgba(151,187,205,1)",
-            data: [28, 48, 40, 19, 86, 27, 90]
-        }
-    ]
-};
-
-return data
-}
-
-*/
 
 // Homepage
 router.get('/', function(req, res) {
@@ -114,11 +117,11 @@ router.get('/camera/:name', function(req, res) {
   console.log('Showing camera: ', req.params.name);
   var cameraName = req.params.name;
   
-  getCameras(function(err, cameras) {
-    getPhotosFromCamera(cameraName, function(err, photoset) {
-      // docs is an array of all the documents in mycollection
-      console.log('Processing camera ', cameraName);
-      res.render('camera', { cameras: cameras || [], cameraName: cameraName, photoset: photoset });
+  getCamera(cameraName, function(err, cameraDoc) {
+    getLenses(cameraName, function(err, lensDocs) {
+      getPhotosFromCamera(cameraName, function(err, photoset) {
+        res.render('camera', { cameraDoc: cameraDoc, lensDocs: lensDocs, cameraName: cameraName, photoset: photoset });
+      });
     });
   });
 });
@@ -127,23 +130,30 @@ router.get('/camera/:name', function(req, res) {
 // photoset given camera and lens
 router.get('/camera/:camera/lens/:lens', function(req, res) {
   console.log('Showing camera: ', req.params.camera, ' and lens: ', req.params.lens)
+  var cameraName = req.params.camera;
+  var lensName = req.params.lens;
   
-  getPhotoset(function(cameras){
-    db.cameras.findOne({name: req.params.name}, function(err, cam) {
-      getPhotoset(cam.camera, cam.lens, function(err, photoset) {
-        // docs is an array of all the documents in mycollection
-        console.log(cam.name);
-        res.render('camera', { cameras:  cameras, cam: cam, photoset: photoset });
+  getCamera(cameraName, function(err, cameraDoc) {
+    getLens(lensName, function(err, lensDoc) {
+      getPhotoset(cameraName, lensName, function(err, photoset) {
+        getFocalLengths(lensName, function(err, focalLengths) {
+          res.render('lenses', { 
+            cameraName: cameraName, 
+            lensName: lensName, 
+            cameraDoc: cameraDoc, 
+            lensDoc: lensDoc, 
+            photoset: photoset, 
+            focalLength: {
+              _id: _.pluck(focalLengths, '_id'),
+              count: _.pluck(focalLengths, 'count')
+            } 
+          });
+        });
       });
-    }); 
- });
+    });
+  });
+
 });
-
-
-
-
-
-
 
 
 router.get('/import', function(req, res) {
